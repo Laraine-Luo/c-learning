138b0ad63a0aa3effc366f229eacc78cc79315e3
Laraine <luvvvryi@gmail.com>
2026-04-07 19:41:11 +0800

    Create Text1.cpp

---
+++ Lesson1/Text1.cpp
@@ -0,0 +1,14 @@
+###题目1:
+##创建一个新项目，编写程序输出你的中文姓名、学号以及你对本门课程的学习目标。 
+##要求每个信息独占一行。
+
+##参考答案:
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<" 姓名:"<<endl;
+    cout<<" 学号:"<<endl;
+    cout<<" 学习目标:好好学习天天向上"<<endl;
+    return 0;
+
+}
